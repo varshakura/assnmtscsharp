5615b8e3317935b676fd85a9c21633cd51f317e6
Kura <SriVarsha.Kura@thomsonreuters.com>
2018-10-04 20:11:41 +0530

    Add project files.

---
+++ assnmtscsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public class Exercise2
+{
+    public static void Main()
+    {
+        int[] ar = new int[100];
+         Console.Write("enter number of elements");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("enter numbers");
+        for (int i = 0; i < n; i++)
+        {
+            ar[i] = Convert.ToInt32(Console.ReadLine());
+
+        }
+            int maxValue = ar.Max();
+
+            Console.Write("greatest number is {0}",  maxValue);
+                   
+
+                Console.ReadKey();
+   
+        }
+
+
+    }
